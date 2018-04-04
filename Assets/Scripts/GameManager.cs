7c959e72392c508adedccd30850637a9411da734
Maxim Kolbowski-Frampton <maxim@tinybop.com>
2018-04-04 19:49:31 -0400

    adding InControl package

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	//singleton stuff here
+	enum GameState {title, playerSelect, gameReady, gameStart, gameEnd};
+	public enum currentGameState;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
